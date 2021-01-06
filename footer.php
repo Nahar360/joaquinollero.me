b8e41c376dae7d4e9738f02dbb26d24918726588
Joaquin <joaquinollerogarcia@gmail.com>
2021-01-06 19:34:03 +0000

    Updated footer to 2021.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <div class="copyright pull-right">
-            2020, made by Joaquin Ollero
+            2021, made by Joaquin Ollero
         </div>
     </div>
 </footer>
